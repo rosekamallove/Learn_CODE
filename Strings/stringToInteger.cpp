4029a844713d0b735aaaee3870d46a7ec10cec4b
rosekamallove <rosekamallove@gmail.com>
2020-11-15 12:14:07 +0530

    ...

---
+++ Strings/stringToInteger.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string str = "34234752934785";
+    int x = stoi(str);
+    cout << x << endl;
+    cout << x + 2 << endl;
+}
