ab68f7d4ceb7e580be549cc500134941d172e2eb
rosekamallove <rosekamallove@gmail.com>
2020-11-18 09:44:28 +0530

    ...

---
+++ bitManipulation/bitManipulationChallenges/isPowerOfTwo.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+bool isPowerOfTwo(int n)
+{
+    return (n && !(n&(n-1)));
+}
+int main()
+{
+    int number;
+    cin>>number;
+    cout << isPowerOfTwo(number)<<endl;
+}
