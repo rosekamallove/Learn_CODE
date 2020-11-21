e14df167a1e9b260bc053296fce75a99cb27ca5e
rosekamallove <rosekamallove@gmail.com>
2020-11-21 15:11:22 +0530

    ...

---
+++ theNewBoston/calcultr.cpp
@@ -0,0 +1,16 @@
+#include<conio.h>
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"enter a number"<<endl;
+	int a;
+	cin>>a;
+	cout <<"enter another number<"<<endl;
+	int b;
+	cin>>b;
+
+	int sum = a+b;
+	cout << sum<<endl;
+}
