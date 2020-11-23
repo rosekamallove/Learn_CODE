b1b3b09f59cc90486644bde3843879e0703329f9
rosekamallove <rosekamallove@gmail.com>
2020-11-23 20:36:33 +0530

    ...

---
+++ sieveOfEratosthenes/primeFactorization.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void primeFactor(int n)
+{
+    int spf[100] = {0};
+    for (int i = 2; i <= n; i++)
+    {
+        spf[i] = i;
+    }
+
+    for (int i = 2; i <= n; i++)
+    {
+        if (spf[i] == i)
+        {
+            for (int j = i * i; i <= n; j += i)
+            {
+                if (spf[j] == j)
+                {
+                    spf[j] = i;
+                }
+            }
+        }
+    }
+
+    while (n != 1)
+    {
+        cout << spf[n] << " ";
+        n = n / spf[n];
+    }
+}
+
+int main()
+{
+    int x;
+    cin >> x;
+
+    primeFactor(x);
+}
