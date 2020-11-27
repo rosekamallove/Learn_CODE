5cf8c6f5dbc67c29403b4ac22ae4f06d379007ee
rosekamalallove <rosekamalallove@gmail.com>
2020-11-27 16:24:09 +0530

    ...

---
+++ Recursion/factorialUsingRecursion.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int factorial(int n)
+{
+    if(n==0)
+    {
+        return 1;
+    }
+
+    int prevFactorial=factorial(n-1);
+    return n*prevFactorial;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<factorial(n)<<endl;
+
+}
