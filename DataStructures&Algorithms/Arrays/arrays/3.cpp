ceccda824b8791bb4a48f566ea7e325b54cb067f
Rose Kamal Love <rosekamallove@gmail.com>
2021-01-03 14:20:37 +0530

    ...

--- DataStructures&Algorithms/Arrays/arrays/3.cpp
+++ DataStructures&Algorithms/Arrays/arrays/3.cpp
@@ -1,32 +1,32 @@
 #include <iostream>
-#include<climits>
+#include <climits>
 using namespace std;
 
 int main()
 {
-   int n;
-   cin >>n;
+    int n;
+    cin >> n;
 
-   int array[n];
-   for(int i=0; i<n; i++)
-   {
-       cin>>array[i];
-   }
+    int array[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> array[i];
+    }
 
-   int maxNo,minNo;
-   maxNo=INT_MIN;
-   minNo=INT_MAX;
+    int maxNo, minNo;
+    maxNo = INT_MIN;
+    minNo = INT_MAX;
 
-   for(int i=0; i<n; i++)
-   {
-       // maxNo=max(maxNo,array[i]);  can replace the if-statement
-       if(array[i]>maxNo)
-       {
-           maxNo=array[i];
-       }
-        if(array[i]>minNo)
-       {
-           minNo=array[i];
-       }
-   }
+    for (int i = 0; i < n; i++)
+    {
+        // maxNo=max(maxNo,array[i]);  can replace the if-statement
+        if (array[i] > maxNo)
+        {
+            maxNo = array[i];
+        }
+        if (array[i] > minNo)
+        {
+            minNo = array[i];
+        }
+    }
 }
