8a5de03c38a27e01dcab5c2f7817bd467b07e8c8
rosekamallove <rosekamallove@gmail.com>
2021-01-09 23:12:46 +0530

    ArrayQuestions

--- Questions/Arrays/Intermediate/q10.cpp
+++ Questions/Arrays/Intermediate/q10.cpp
@@ -1,5 +1,7 @@
 /*
-move all the elements to one side of the array
+Given an array with all distinct elements, find the largest three elements. 
+Expected time complexity is O(n) and extra space is O(1).
+Input: arr[] = {10, 4, 3, 50, 23, 90}Output: 90, 50, 23
 */
 #include <bits/stdc++.h>
 using namespace std;
