4ec17fbaa02b78f3b2188f4d9ab7da17f07f46d4
rosekamallove <rosekamallove@gmail.com>
2021-01-09 22:13:03 +0530

    ArrayQuestions

---
+++ Questions/Arrays/Basic/q6.cpp
@@ -0,0 +1,10 @@
+/*
+Find the kth largest and kth smallest number in an array
+*/
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    return 0;
+}
---
+++ Questions/Arrays/Intermediate/q5.cpp
@@ -0,0 +1,62 @@
+/*
+link:https://practice.geeksforgeeks.org/problems/find-duplicates-in-an-array/1
+Given an array a[] of size N which contains elements from 0 to N-1, you need to find all the elements occurring more than once in the given array.
+
+Example 1:
+
+Input:
+N = 4
+a[] = {0,3,1,2}
+Output: -1
+Explanation: N=4 and all elements from 0
+to (N-1 = 3) are present in the given
+array. Therefore output is -1.
+Example 2:
+
+Input:
+N = 5
+a[] = {2,3,1,2,3}
+Output: 2 3 
+Explanation: 2 and 3 occur more than once
+in the given array.
+Your Task:
+Complete the function duplicates() which takes array a[] and n as input as parameters and returns a list of elements that occur more than once in the given array in sorted manner. If no such element is found return -1. 
+
+Expected Time Complexity: O(n).
+Expected Auxiliary Space: O(n).
+Note : The extra space is only for the array to be returned.
+Try and perform all operation withing the provided array. 
+
+Constraints:
+1 <= N <= 105
+0 <= A[i] <= N-1, for each valid i
+*/
+
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> duplicates(int arr[], int n);
+int main()
+{
+    int t;
+    cin >> t;
+    while (t-- > 0)
+    {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+        vector<int> ans = duplicates(a, n);
+        for (int i : ans)
+            cout << i << ' ';
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+vector<int> duplicates(int a[], int n)
+{
+    // code here
+}
