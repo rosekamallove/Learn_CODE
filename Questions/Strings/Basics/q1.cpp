7372fc140aaef3c7054c6d466526f3e2953429d1
rosekamallove <rosekamallove@gmail.com>
2021-01-16 10:37:57 +0530

    StringBasicQuestions

---
+++ Questions/Strings/Basics/q1.cpp
@@ -0,0 +1,12 @@
+/* 
+write a basic programm to take input (String) form user 
+and print it.
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    
+    return 0;
+}
---
+++ Questions/Strings/Basics/q8.cpp
@@ -0,0 +1,9 @@
+//  How do you convert string into int and int into string
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    
+    return 0;
+}
